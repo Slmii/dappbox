d12c2da64b00f323831e59284d248939872b9005
Slmii <selami.c92@hotmail.com>
2022-12-15 22:00:01 +0100

    open non image assets in new tab for preview

--- src/components/Actions/Preview/PreviewBackdrop.component.tsx
+++ src/components/Actions/Preview/PreviewBackdrop.component.tsx
@@ -1,10 +1,12 @@
+import Paper from '@mui/material/Paper';
 import { useMemo, useState } from 'react';
 
 import { constants } from 'lib/constants';
 import { useKeyPress } from 'lib/hooks';
 import { Doc } from 'lib/types/Doc.types';
 import { Backdrop } from 'ui-components/Backdrop';
-import { Box, Column } from 'ui-components/Box';
+import { Box, Column, Row } from 'ui-components/Box';
+import { Button } from 'ui-components/Button';
 import { IconButton } from 'ui-components/IconButton';
 import { Paragraph } from 'ui-components/Typography';
 
@@ -57,16 +59,17 @@ export const PreviewBackdrop = ({ open, docs, onClick }: { open: boolean; docs: 
 			>
 				<Box sx={{ display: 'flex', justifyContent: 'space-between' }}>
 					<Column>
-						<IconButton icon='close' label='Close' />
+						<IconButton icon='close' label='Close' color='inherit' />
 						<Paragraph>{docToUse?.name}</Paragraph>
 					</Column>
 					<Column>
-						<IconButton icon='download' label='Download' />
-						<IconButton icon='print' label='Print' onClick={() => window.print()} />
+						<IconButton icon='download' label='Download' color='inherit' />
+						<IconButton icon='print' label='Print' onClick={() => window.print()} color='inherit' />
 						<IconButton
 							icon='newWindow'
 							label='Open in new window'
 							onClick={() => window.open(docToUse.url, '_blank')}
+							color='inherit'
 						/>
 					</Column>
 				</Box>
@@ -84,6 +87,7 @@ export const PreviewBackdrop = ({ open, docs, onClick }: { open: boolean; docs: 
 						label='Previous'
 						onClick={handleOnPrevious}
 						sx={{ marginRight: theme => theme.spacing(constants.SPACING) }}
+						color='inherit'
 					/>
 				) : null}
 				<Box
@@ -105,7 +109,27 @@ export const PreviewBackdrop = ({ open, docs, onClick }: { open: boolean; docs: 
 							alt={docToUse.name}
 						/>
 					) : null}
-					{docToUse && docToUse.mimeType.includes('pdf') ? <h1>Open in new tab</h1> : null}
+					{docToUse && docToUse.mimeType.includes('pdf') ? (
+						<Paper
+							elevation={10}
+							sx={{
+								padding: theme => theme.spacing(constants.SPACING),
+								backgroundColor: 'black',
+								borderRadius: theme => theme.shape.borderRadius,
+								color: 'white'
+							}}
+						>
+							<Row>
+								<Paragraph>Non-Image assets are not yet supported for Preview!</Paragraph>
+								<div style={{ textAlign: 'center' }}>
+									<Button
+										label='Open in new window'
+										onClick={() => window.open(docToUse.url, '_blank')}
+									/>
+								</div>
+							</Row>
+						</Paper>
+					) : null}
 				</Box>
 				{docs.length > 1 ? (
 					<IconButton
@@ -113,6 +137,7 @@ export const PreviewBackdrop = ({ open, docs, onClick }: { open: boolean; docs: 
 						label='Next'
 						onClick={handleOnNext}
 						sx={{ marginLeft: theme => theme.spacing(constants.SPACING) }}
+						color='inherit'
 					/>
 				) : null}
 			</Box>
