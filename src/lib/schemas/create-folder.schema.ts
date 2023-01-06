22e665bfe71160b6df9be5a42a1a1cb3e5d3a8e1
Slmii <selami1992@gmail.com>
2023-01-06 23:52:52 +0100

    add some regex for folder creation and renaming

--- src/lib/schemas/create-folder.schema.ts
+++ src/lib/schemas/create-folder.schema.ts
@@ -3,6 +3,10 @@ import * as z from 'zod';
 export const createFolderSchema = z.object({
 	folderName: z
 		.string()
-		.min(2, { message: 'Folder name must be at least 2 characters long' })
-		.nonempty({ message: 'Folder name must be at least 2 characters long' })
+		.min(2, { message: 'Folder names must have a minimum length of 2 characters' })
+		.regex(
+			/^[a-zA-Z0-9-_.\s]+$/,
+			'Folder names can only contain alphanumeric characters, hyphens, underscores or spaces'
+		)
+		.nonempty({ message: 'Folder names must have a minimum length of 2 characters' })
 });
--- src/lib/schemas/rename-folder.schema.ts
+++ src/lib/schemas/rename-folder.schema.ts
@@ -3,6 +3,10 @@ import * as z from 'zod';
 export const renameFolderSchema = z.object({
 	folderName: z
 		.string()
-		.min(2, { message: 'Asset name must be at least 2 characters long' })
-		.nonempty({ message: 'Asset name must be at least 2 characters long' })
+		.min(2, { message: 'Asset names must have a minimum length of 2 characters' })
+		.regex(
+			/^[a-zA-Z0-9-_.\s]+$/,
+			'Asset names can only contain alphanumeric characters, hyphens, underscores or spaces'
+		)
+		.nonempty({ message: 'Asset names must have a minimum length of 2 characters' })
 });
